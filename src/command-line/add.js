aca5c309aa28f52b77cd6b2fcdb90220a7ecbde2
Pavel Djundik <github@xpaw.me>
2017-07-18 13:28:07 +0300

    Add simple setup questions on first run

--- src/command-line/add.js
+++ src/command-line/add.js
@@ -14,39 +14,54 @@ program
 			return;
 		}
 
-		const ClientManager = require("../clientManager");
+		prompt(name);
+	});
 
-		var manager = new ClientManager();
-		var users = manager.getUsers();
-		if (users.indexOf(name) !== -1) {
-			log.error(`User ${colors.bold(name)} already exists.`);
+function prompt(name, callback) {
+	const ClientManager = require("../clientManager");
+
+	var manager = new ClientManager();
+	var users = manager.getUsers();
+
+	if (users.indexOf(name) !== -1) {
+		log.error(`User ${colors.bold(name)} already exists.`);
+		return;
+	}
+
+	log.prompt({
+		text: "Enter password:",
+		silent: true
+	}, function(err, password) {
+		if (!password) {
+			log.error("Password cannot be empty.");
 			return;
 		}
+
+		if (err) {
+			return;
+		}
+
 		log.prompt({
-			text: "Enter password:",
-			silent: true
-		}, function(err, password) {
-			if (!password) {
-				log.error("Password cannot be empty.");
+			text: "Save logs to disk?",
+			default: "yes"
+		}, function(err2, enableLog) {
+			if (err2) {
 				return;
 			}
-			if (!err) {
-				log.prompt({
-					text: "Save logs to disk?",
-					default: "yes"
-				}, function(err2, enableLog) {
-					if (!err2) {
-						add(
-							manager,
-							name,
-							password,
-							enableLog.charAt(0).toLowerCase() === "y"
-						);
-					}
-				});
+
+			add(
+				manager,
+				name,
+				password,
+				enableLog.charAt(0).toLowerCase() === "y"
+			);
+
+			if (callback) {
+				callback();
 			}
 		});
 	});
+}
 
 function add(manager, name, password, enableLog) {
 	var hash = Helper.password.hash(password);
@@ -55,3 +70,5 @@ function add(manager, name, password, enableLog) {
 	log.info(`User ${colors.bold(name)} created.`);
 	log.info(`User file located at ${colors.green(Helper.getUserConfigPath(name))}.`);
 }
+
+module.exports.prompt = prompt;
--- src/command-line/start.js
+++ src/command-line/start.js
@@ -14,50 +14,128 @@ program
 	.option("-B, --bind <ip>", "set the local IP to bind to for outgoing connections")
 	.option("    --public", "start in public mode")
 	.option("    --private", "start in private mode")
+	.option("    --skip-setup", "skip configuration questions")
 	.description("Start the server")
 	.action(function(options) {
-		initalizeConfig();
-
-		const ClientManager = require("../clientManager");
-		const server = require("../server");
-
-		var users = new ClientManager().getUsers();
-
-		var mode = Helper.config.public;
 		if (options.public) {
-			mode = true;
+			Helper.config.public = true;
 		} else if (options.private) {
-			mode = false;
-		}
-
-		if (!mode && !users.length && !Helper.config.ldap.enable) {
-			log.warn("No users found.");
-			log.info(`Create a new user with ${colors.bold("lounge add <name>")}.`);
-
-			return;
+			Helper.config.public = false;
 		}
 
 		Helper.config.host = options.host || Helper.config.host;
 		Helper.config.port = options.port || Helper.config.port;
 		Helper.config.bind = options.bind || Helper.config.bind;
-		Helper.config.public = mode;
 
-		server();
+		initalizeConfig(options.skipSetup, startServer);
 	});
 
-function initalizeConfig() {
-	if (!fs.existsSync(Helper.CONFIG_PATH)) {
-		fsextra.ensureDirSync(Helper.HOME);
-		fs.chmodSync(Helper.HOME, "0700");
-		fsextra.copySync(path.resolve(path.join(
-			__dirname,
-			"..",
-			"..",
-			"defaults",
-			"config.js"
-		)), Helper.CONFIG_PATH);
-		log.info(`Configuration file created at ${colors.green(Helper.CONFIG_PATH)}.`);
+function initalizeConfig(skipSetup, callback) {
+	fsextra.ensureDirSync(Helper.USERS_PATH);
+
+	if (fs.existsSync(Helper.CONFIG_PATH)) {
+		return callback();
 	}
 
-	fsextra.ensureDirSync(Helper.USERS_PATH);
+	let config = fs.readFileSync(path.join(
+		__dirname,
+		"..",
+		"..",
+		"defaults",
+		"config.js"
+	), "utf8");
+
+	if (skipSetup || !process.stdout.isTTY || process.env.CI) {
+		log.info("Skipping setup and using default config.");
+
+		writeConfig(config, callback);
+		return;
+	}
+
+	log.info("We are now going to configure The Lounge.");
+
+	log.prompt({
+		text: "What port would you like The Lounge to run on?",
+		default: 9000
+	}, (a, port) => {
+		port = parseInt(port, 10) || 9000;
+		config = config.replace("port: 9000,", `port: ${port},`);
+		Helper.config.port = port;
+
+		console.log("");
+
+		log.info("The Lounge has two modes, public and private.");
+		log.info("In public mode, anyone can use the instance and connect to IRC. All IRC connections are closed when the page is closed.");
+
+		console.log("");
+
+		log.info(`In private mode, user account is required, which can be created with ${colors.bold("lounge add <name>")} command.`);
+		log.info("IRC connections are kept alive for all users even when the page is closed.");
+		log.prompt({
+			text: "Enable user authentication?",
+			default: "yes"
+		}, (b, mode) => {
+			mode = mode.charAt(0).toLowerCase() === "y";
+
+			Helper.config.public = !mode;
+
+			if (!mode) {
+				writeConfig(config, callback);
+				return;
+			}
+
+			config = config.replace("public: true,", "public: false,");
+
+			console.log("");
+
+			log.info("You chose private instance, now we are going to create the first user.");
+			log.prompt({
+				text: "Enter username:"
+			}, function(c, username) {
+				if (!username) {
+					log.error("Username cannot be empty.");
+					return;
+				}
+
+				const add = require("./add");
+				add.prompt(username, () => {
+					writeConfig(config, callback);
+				});
+			});
+		});
+	});
+}
+
+function writeConfig(config, callback) {
+	if (fs.existsSync(Helper.CONFIG_PATH)) {
+		return callback();
+	}
+
+	fsextra.ensureDirSync(Helper.HOME);
+	fs.chmodSync(Helper.HOME, "0700");
+	fs.writeFileSync(Helper.CONFIG_PATH, config);
+
+	console.log("");
+
+	log.info(`Configuration file created at ${colors.green(Helper.CONFIG_PATH)}`);
+	log.info("This configuration file has a lot more options, you should check it out and configure The Lounge for your needs!");
+
+	console.log("");
+
+	callback();
+}
+
+function startServer() {
+	const ClientManager = require("../clientManager");
+	const users = new ClientManager().getUsers();
+	const server = require("../server");
+
+	if (!Helper.config.public && !users.length && !Helper.config.ldap.enable) {
+		log.warn("No users found.");
+		log.info(`Create a new user with ${colors.bold("lounge add <name>")}.`);
+
+		return;
+	}
+
+	server();
 }
